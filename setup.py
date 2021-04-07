e0f656a428a127635a16d13f66e5ea72e5114a59
benno1237 <35632314+benno1237@users.noreply.github.com>
2021-04-07 18:10:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author = "benno1237, honigkuchen",
     author_email = "benno.kollreider@gmail.com",
     url = "https://github.com/benno1237/MinePI",
-    download_url = "",
+    download_url = "https://github.com/benno1237/MinePI/archive/refs/tags/0.1.tar.gz",
     keywords = ["Minecraft", "Skin", "Render", "Head", "UUID"],
     install_requires = [
         "aiohttp",
