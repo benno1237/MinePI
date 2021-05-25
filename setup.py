18068001e9ccd75edc768be053d0451877b73a7d
benno1237 <benno.kollreider@gmail.com>
2021-05-25 16:37:51 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "Pillow"
     ],
       classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',     
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',  
