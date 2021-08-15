6326b674fa6e5e1bab34f3cc66ccc3412d8c2028
benno1237 <35632314+benno1237@users.noreply.github.com>
2021-08-15 16:13:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
     install_requires = [
         "aiohttp",
         "Pillow",
-        "sphinx-prompt",
-        
+        "sphinx-prompt"
     ],
       classifiers=[
     'Development Status :: 3 - Alpha',     
